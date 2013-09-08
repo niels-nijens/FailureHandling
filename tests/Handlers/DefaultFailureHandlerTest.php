156eee73c13424772862a54017f40881f5c5c092
Niels Nijens <nijens.niels@gmail.com>
2013-09-08 22:37:11 +0200

    Added unit tests for DefaultFailureHandler

--- tests/Handlers/DefaultFailureHandlerTest.php
+++ tests/Handlers/DefaultFailureHandlerTest.php
@@ -2,6 +2,8 @@
 
 namespace AtomicPHP\FailureHandling\Tests;
 
+use \Exception;
+use \AtomicPHP\FailureHandling\Exceptions\ErrorException;
 use \AtomicPHP\FailureHandling\Handlers\DefaultFailureHandler;
 
 /**
@@ -26,5 +28,44 @@ class DefaultFailureHandlerTest extends \PHPUnit_Framework_TestCase
     public function testHandleError() {
         $failureHandler = new DefaultFailureHandler();
         $failureHandler->handleError(E_ERROR, "Error message", __FILE__, __LINE__);
+    }
+
+    /**
+     * testHandleException
+     *
+     * Tests if DefaultFailureHandler::handleException logs the exception to the instance implementing LoggerInterface
+     *
+     * @access public
+     * @return void
+     **/
+    public function testHandleException() {
+        $failureHandler = new DefaultFailureHandler();
+
+        $mockLogger = $this->getMock("\Psr\Log\LoggerInterface");
+
+        $failureHandler->setLogger($mockLogger);
+        $failureHandler->handleException(new Exception("Exception message") );
+    }
+
+    /**
+     * testHandleExceptionAfterHandleError
+     *
+     * Tests if the Exception thrown in DefaultFailureHandler::handleError is logged correctly to the instance implementing LoggerInterface
+     *
+     * @access public
+     * @return void
+     **/
+    public function testHandleExceptionAfterHandleError() {
+        $mockLogger = $this->getMock("\Psr\Log\LoggerInterface");
+
+        $failureHandler = new DefaultFailureHandler();
+        $failureHandler->setLogger($mockLogger);
+        
+        try {
+            $failureHandler->handleError(E_ERROR, "Error message", __FILE__, __LINE__);
+        }
+        catch (ErrorException $exception) {
+            $failureHandler->handleException($exception);
+        }
     }
 }
